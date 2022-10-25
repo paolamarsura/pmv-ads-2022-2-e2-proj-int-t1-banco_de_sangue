6dd2c0b3fa9b876cec7e36ba4fdef20b725926f7
paolamarsura <paolamarsura@gmail.com>
2022-10-25 19:45:17 -0300

    apagar, editar e lista de instituicao

--- BancoDeSangue/BancoDeSangue/Controllers/InstituicaoController.cs
+++ BancoDeSangue/BancoDeSangue/Controllers/InstituicaoController.cs
@@ -11,10 +11,24 @@ namespace BancoDeSangue.Controllers
     public class InstituicaoController : Controller     
     {
         private readonly IInstituicaoRepositorio _instituicaoRepositorio;
+        private object _InstituicaoRepositorio;
+        private object instituicao;
 
         public InstituicaoController(IInstituicaoRepositorio instituicaoRepositorio)
         { 
             _instituicaoRepositorio = instituicaoRepositorio;
+        }
+
+       
+        public IActionResult Cadastrar()
+        {
+            return View();
+            
+        }
+
+         public IActionResult Editar()
+        {
+            return View();
         }
 
         public IActionResult Index()
@@ -22,18 +36,27 @@ namespace BancoDeSangue.Controllers
             return View();
         }
 
-        public IActionResult Cadastrar()
+        public IActionResult Apagar()
         {
             return View();
         }
-        
+
+        public IActionResult ListaDeInstituicao()
+        {
+            var teste = _instituicaoRepositorio.BuscarTodos();
+            return View("ListaDeInstituicao", teste);
+        }
+
+
+
         [HttpPost]
         public IActionResult Cadastrar(InstituicaoModel instituicao)
         {
             _instituicaoRepositorio.Adicionar(instituicao);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("ListaDeInstituicao", "Instituicao");
 
         }
-    }
+
+     }
          
 }
