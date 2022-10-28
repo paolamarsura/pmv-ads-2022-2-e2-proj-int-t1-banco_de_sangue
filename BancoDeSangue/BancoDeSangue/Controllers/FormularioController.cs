f99fdb3754d643a58fcf9997a97d1cfa4830197f
paolamarsura <paolamarsura@gmail.com>
2022-10-28 13:49:11 -0300

    Tela de formulário

---
+++ BancoDeSangue/BancoDeSangue/Controllers/FormularioController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BancoDeSangue.Controllers
+{
+    public class Formulario : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+    }
+}
---
+++ BancoDeSangue/BancoDeSangue/Models/FormularioModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BancoDeSangue.Models
+{
+    public class FormularioModel
+    {
+        public bool idade { get; set; }
+
+        public String sexo { get; set; }
+
+        public bool peso { get; set; }
+
+        public bool gripeProcessoInfeccioso { get; set; }
+
+        public bool tatoo { get; set; }
+
+        public bool jejum { get; set; }
+
+        public bool malariaChagas { get; set; }
+
+        public bool hepatite { get; set; }
+
+        public bool riscoHIV { get; set; }
+
+        public bool procCirurgico { get; set; }
+
+    }
+
+}
