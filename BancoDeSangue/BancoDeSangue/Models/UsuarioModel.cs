ef00f62765508d51c2613d4306e1a25977320dda
Ríder Rocha Cantuária <rider.cantuaria@simply-vc.com.mt>
2022-11-25 22:53:38 -0400

    Ajustando Model de Formulario

--- BancoDeSangue/BancoDeSangue/Models/UsuarioModel.cs
+++ BancoDeSangue/BancoDeSangue/Models/UsuarioModel.cs
@@ -1,27 +1,43 @@
 ﻿using BancoDeSangue.Helper;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace BancoDeSangue.Models
 {
     public class UsuarioModel
     {
-        [Key]
 
+        [Key]
+        public int id{ get; set; }
+
+        [Required]
         public String nome { get; set; }
-        
+
+        [Required]
         public String email { get; set; }
 
+        [Required]
         public String senha { get; set; }
 
         public String perfil { get; set; }
 
+        public DateTime criacao { get; set; }
+
+        [NotMapped]
+        public String erro{ get; set; }
+
         public void SetSenhaHash(UsuarioModel usuario)
         {
             this.senha = usuario.senha.GerarHash();
+        }
+
+        public void SetPerfil(UsuarioModel usuario)
+        {
+            if (usuario.perfil == null) {
+                this.perfil = "USER";
+            }            
         }
     }
 }
